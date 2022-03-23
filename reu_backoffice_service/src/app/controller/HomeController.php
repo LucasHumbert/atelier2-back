ef2e07bf4031b831c4a70e2d1c61f9b8e524e282
Lucas Humbert <lucas.humbert.28@gmail.com>
2022-03-23 18:20:14 +0100

    wip

---
+++ reu_backoffice_service/src/app/controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace reu\backoffice\app\controller;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use reu\backoffice\app\model\Event;
+use reu\backoffice\app\model\User;
+use reu\backoffice\app\utils\Writer;
+
+class HomeController
+{
+    public function getInfos(Request $request, Response $response): Response
+    {
+        $users = User::all();
+        $usersCount = count($users);
+
+        $events = Event::all();
+        $eventsCount = count($events);
+
+        return Writer::jsonOutput($response, 200, ['nbUsers' => $usersCount, 'nbEvents' => $eventsCount]);
+    }
+}
