6d8569d3d78236bb4a79fa3293043d57487300d1
baptistehouques <baptistehouques38@gmail.com>
2022-03-24 15:06:39 +0100

    :zap:change eventUsers

--- reu_event_service/src/app/controller/UserController.php
+++ reu_event_service/src/app/controller/UserController.php
@@ -2,15 +2,25 @@
 
 namespace reu\event\app\controller;
 
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use reu\event\app\model\Event;
 use reu\event\app\model\User;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use reu\event\app\utils\Writer;
+use Slim\Container;
 
 class UserController
 {
+    private $c;
+
+    public function __construct(Container $c)
+    {
+        $this->c = $c;
+    }
+
     public function getUsers(Request $request, Response $response)
     {
         $users = User::all();
@@ -20,8 +30,15 @@ class UserController
     public function getUsersEvents(Request $request, Response $response, $args)
     {
         try {
+            $tokenstring = $args['token'];
+            $token = JWT::decode($tokenstring, new Key($this->c['secret'], 'HS512'));
+        }
+        catch (\Exception $e){
+            return Writer::jsonOutput($response, 403, ['message' => $e]);
+        }
+        try {
             $user = User::with('events')
-                ->where('id','=',$args['id'])
+                ->where('id','=',$token->upr->id)
                 ->first();
             $res = [];
             foreach($user->events as $event_utilisateur){
@@ -31,7 +48,7 @@ class UserController
 
 
         } catch (ModelNotFoundException $e) {
-            return Writer::jsonOutput($response, 404, ['message' => 'Expertise introuvable']);
+            return Writer::jsonOutput($response, 404, ['message' => 'Events introuvable']);
         }
 
         return Writer::jsonOutput($response, 200, ['events' => $res]);
